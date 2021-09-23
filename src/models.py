2e433fa0cafe6f29191ef061d9578332306b3243
SennSzl <74777070+SennSzl@users.noreply.github.com>
2021-09-23 14:58:37 +0200

    commit

---
+++ src/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from flask import Flask, request, abort
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:\\Users\\Senn3\\Desktop\\programming\\Projects\\L2_Challenge2\\src\\timestamps.db"
+db = SQLAlchemy(app)
+
+class Timestamps(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.Dateti)
---
+++ src/webhook.py
@@ -0,0 +1,15 @@
+import discord_webhook
+import requests
+import json
+from datetime import datetime
+from discord_webhook import DiscordWebhook
+webhook_url = "http://127.0.0.1:5000/webhook"
+discord_webhook_url = "https://discord.com/api/webhooks/890539825608945684/TAKgjjrMNvzCp3Ehx7UiEbQ9nQLWZMzU_GbhTTMVv169-beJpkiYsvpCQcgTSZ54rg10"
+
+timestamp = datetime.timestamp(datetime.now())
+data = {"timestamp": timestamp}
+r = requests.post(webhook_url, data=json.dumps(data), headers={"Content-type": "application/json"})
+
+
+#discord_webhook = DiscordWebhook(url=discord_webhook_url, content=str(timestamp))
+#response = discord_webhook.execute()
