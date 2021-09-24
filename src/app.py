3dcc63a1745edc02f6ffb7b29798760c7f8df176
SennSzl <74777070+SennSzl@users.noreply.github.com>
2021-09-24 15:03:19 +0200

    webhook fully done with mongodb

--- src/app.py
+++ src/app.py
@@ -1,14 +1,31 @@
+from datetime import time
+from os import times
 from flask import Flask, render_template ,request, abort
-
+from pymongo import MongoClient
+import json
+#connect to database
+conn = MongoClient("localhost", 27017)
+db = conn.local
+col = db.timestamps
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     if request.method == "POST":
-        print(request.json)
+        try:
+            timestamp = request.json
+            if type(timestamp["timestamp"]) == float:
+                col.insert_one(timestamp)
+                col_data = col.find({})
+                for x in col_data:
+                    print(x["timestamp"])
+            else:
+                raise TypeError
+        except Exception:
+            print("Invalid webhook data")
         return "success",  200
     else:
         abort(400)
